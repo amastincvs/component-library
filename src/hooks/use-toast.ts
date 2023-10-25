7f9f418340350f9c724708417e637b035bd6d12e
Alex Mastin <alexander.mastin@cvshealth.com>
2023-10-25 15:02:35 -0500

    fix: fixed issue causing build error

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,6 +1,5 @@
 // Inspired by react-hot-toast library
-import * as React from 'react'
-
+import { type ReactNode, useEffect, useState } from 'react'
 import type { ToastActionElement, ToastProps } from '../components'
 
 const TOAST_LIMIT = 1
@@ -8,8 +7,8 @@ const TOAST_REMOVE_DELAY = 1000000
 
 type ToasterToast = ToastProps & {
   id: string
-  title?: React.ReactNode
-  description?: React.ReactNode
+  title?: ReactNode
+  description?: ReactNode
   action?: ToastActionElement
 }
 
@@ -167,9 +166,9 @@ function toast({ ...props }: Toast) {
 }
 
 function useToast() {
-  const [state, setState] = React.useState<State>(memoryState)
+  const [state, setState] = useState<State>(memoryState)
 
-  React.useEffect(() => {
+  useEffect(() => {
     listeners.push(setState)
     return () => {
       const index = listeners.indexOf(setState)
