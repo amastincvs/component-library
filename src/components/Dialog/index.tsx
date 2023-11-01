96a60d01d4d758d36e22684656e71b4162454867
Alex Mastin <alexander.mastin@cvshealth.com>
2023-11-01 11:24:18 -0500

    fix: added props to dialog close component

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -40,14 +40,16 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 const DialogClose = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Close>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Close>
->(({ className, ...props }, ref) => (
+>(({ children, className, ...props }, ref) => (
   <DialogPrimitive.Close
+    ref={ref}
     className={cn(
       'absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground',
       className
     )}
+    {...props}
   >
-    {props.children}
+    {children}
     <span className="sr-only">Close</span>
   </DialogPrimitive.Close>
 ))
