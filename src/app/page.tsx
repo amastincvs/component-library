aacbf42f5b6449b5511f2a38bc34b2d1e62b9375
Alex Mastin <alexander.mastin@cvshealth.com>
2023-12-07 09:48:45 -0600

    feature: added tooltip to theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Header, Input } from '~/components'
+import { Header, HeaderThemeToggle, Input } from '~/components'
 
 export default function Playground() {
   return (
     <div className="flex flex-col">
-      <Header name="Component Library Playground" />
+      <Header name="Component Library Playground">
+        <HeaderThemeToggle />
+      </Header>
       <div className="h-full w-full">
         <Input />
       </div>
